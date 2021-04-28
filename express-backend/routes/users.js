fb71e6faa8d1c5f423457060b4482b5c50d407a9
Logan Emmert <loganlemmert@gmail.com>
2021-04-28 15:34:58 -0600

    made react and express components

--- express-backend/routes/users.js
+++ express-backend/routes/users.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('respond with a resource');
+  res.json([{
+    id:1,
+    username: "SamSmith"
+  },
+{
+  id: 2,
+  username: "LoganEmm"
+},
+{
+  id: 3,
+  username: "Sam Rockwell"
+}
+  ]);
 });
 
 module.exports = router;
